a43cd5fa399e23bca3362013f38051122e4d0b4d
POXZASY <32286007+POXZASY@users.noreply.github.com>
2024-10-23 20:42:00 -0400

    added some comments for 66

--- Project_Euler_Solutions/Solution_66.ml
+++ Project_Euler_Solutions/Solution_66.ml
@@ -1,11 +1,21 @@
 (* Returns true iff the integer is a square. *)
 let is_square i = sqrt(float_of_int i) == float_of_int(int_of_float(sqrt(float_of_int i)))
 
-(* Returns the minimal x satisfying x^2 - dy^2 = 1.
-TODO *)
+(**)
+
+(* TODO
+Returns the minimal x satisfying x^2 - dy^2 = 1.
+https://en.wikipedia.org/wiki/Pell%27s_equation#Fundamental_solution_via_continued_fractions
+Consider the continued fraction sqrt(d).
+Find the sequence of converging fractions h_i/k_i
+Find the length of the period p of the continued fraction.
+The fundamental solution (x_1, y_1) =
+(h_p-1, k_p-1) for p even
+(h_2p-1, k_2p-1) for p odd
+*)
 let minimal_x (d : int) : int = d
 
-(* Returns the D<=1000 of the largest minimal x. *)
+(* Returns the D <= 1000 with the largest minimal x. *)
 let rec largest_minimal_x_d current_d largest_d largest_minimal_x =
   if current_d > 1000 then
     largest_d
