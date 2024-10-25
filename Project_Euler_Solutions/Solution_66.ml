1266ea232fbd0562bce4e37d9cf0fc6334d6d594
POXZASY <32286007+POXZASY@users.noreply.github.com>
2024-10-24 20:34:13 -0400

    library

--- Project_Euler_Solutions/Solution_66.ml
+++ Project_Euler_Solutions/Solution_66.ml
@@ -1,14 +1,22 @@
 (* Returns true iff the integer is a square. *)
 let is_square i = sqrt(float_of_int i) == float_of_int(int_of_float(sqrt(float_of_int i)))
 
-(**)
+(*
+Construct a list of the first p-1 constants if p is even, or 2p-1 if p is odd.
+Element p has the value 2 * floor(d)
+
+Computing the constants of a CF using integers:
+https://math.stackexchange.com/a/4425617
+*)
+let rec cf_constants (d: int) (a0 : int) (current_r : int) (current_s : int) (current_lst : int list) : int list =
+  
 
 (* TODO
 Returns the minimal x satisfying x^2 - dy^2 = 1.
 https://en.wikipedia.org/wiki/Pell%27s_equation#Fundamental_solution_via_continued_fractions
 Consider the continued fraction sqrt(d).
-Find the sequence of converging fractions h_i/k_i
-Find the length of the period p of the continued fraction.
+Generate the list (a1, a2, ...) of constants, and use it to find the length of the period p of the continued fraction.
+Use the list to compute the desired convergent fraction recursively.
 The fundamental solution (x_1, y_1) =
 (h_p-1, k_p-1) for p even
 (h_2p-1, k_2p-1) for p odd
